19df26f6d3aaba5b86082b58d81b414d5feb6c74
Mariusz Libera <mariusz.libera@gmail.com>
2025-03-25 18:54:02 +0100

    more leeway

--- cycle_pause.lua
+++ cycle_pause.lua
@@ -1,6 +1,6 @@
 local function cycle_pause()
     if mp.get_property_bool("pause") then
-        if math.abs(mp.get_property_number("time-pos", 0) - mp.get_property_number("duration", 0)) < 0.15 then
+        if math.abs(mp.get_property_number("time-pos", 0) - mp.get_property_number("duration", 0)) < 0.20 then
             mp.set_property("percent-pos", "0")
         end
         mp.set_property("pause", "no")
